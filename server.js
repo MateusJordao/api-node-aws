a6698ea9ad8b4557b8f9d80f64ac2d35f1eaa1bf
Ubuntu <ubuntu@ip-172-31-95-8.ec2.internal>
2024-11-26 01:20:56 +0000

    I created a sample array to test the request by ID. I also created the function buscarPostId and a new app.get to search by ID.

--- server.js
+++ server.js
@@ -1,11 +1,56 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Uma paisagem montanhosa",
+        imagem: "https://source.unsplash.com/random/300x200"
+    },
+    {
+        id: 3,
+        descricao: "Um prato delicioso de comida",
+        imagem: "https://source.unsplash.com/food/300x200"
+    },
+    {
+        id: 4,
+        descricao: "Um carro esportivo",
+        imagem: "https://source.unsplash.com/cars/300x200"
+    },
+    {
+        id: 5,
+        descricao: "Um cachorro brincando",
+        imagem: "https://source.unsplash.com/dogs/300x200"
+     },
+     {
+        id: 6,
+        descricao: "Uma cidade à noite",
+        imagem: "https://source.unsplash.com/city/300x200"
+     }
+];
+
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api-imersao", (req, res) => {
-    res.status(200).send("Servidor de pé!");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostId(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+};
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostId(req.params.id)
+    res.status(200).json(posts[index]);
 });
